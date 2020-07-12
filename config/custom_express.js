6300569399e6551baf73c8eb44cf8f3e36856e3e
RobertoCarnauba <betocarnauba@hotmail.com>
2020-07-12 18:54:57 -0300

    Implementando validações na entrada de dados.

--- config/custom_express.js
+++ config/custom_express.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
-//var expressValidator = require('express-validator');
+const { check } = require('express-validator');
 
 module.exports = function(){
   var app = express();
 
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-
-  //app.use(expressValidator());
+  app.use(check());
 
   consign()
    .include('controllers')
--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,12 +1,24 @@
 const PagamentoDao = require("../persistencia/PagamentoDao");
+const { body, validationResult } = require('express-validator');
+
 
 
 module.exports = function(app){
+  
     app.get('/pagamentos', (req, res) => {
       res.send('PAGAMENTOS')
     })
-
-    app.post("/pagamentos/pagamento",function(req, res) {
+    
+    app.post('/pagamentos/pagamento', [
+      body('forma_de_pagamento', '[forma_de_pagamento] is required').notEmpty(),
+      body('valor','[valor] is required').isLength({ min: 5 }).isFloat()
+    ], (req, res) => {
+      
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+    
       var pagamento = req.body;
       console.log('processando pagamento...');
   
@@ -16,11 +28,41 @@ module.exports = function(app){
       pagamento.status = "CRIADO";
       pagamento.data = new Date;
   
-      pagamentoDao.salva(pagamento, function(exception, result){
-        console.log('pagamento criado: '+ pagamento.valor);
-        res.json(pagamento);
-      });
+      pagamentoDao.salva(pagamento, function(error, result){
+        if(error){
+          console.log("Erro ao salvar -->"+error)
+          res.status(400).send(error);
+        } else {
+          console.log('pagamento criado: '+ pagamento.valor);
+          res.json(pagamento);
+        }
+
+      });;
     });
+
+    // app.post("/pagamentos/pagamento",function(req, res) {
+
+
+    //   var pagamento = req.body;
+    //   console.log('processando pagamento...');
+  
+    //   var connection = app.persistencia.connectionFactory();
+    //   var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+  
+    //   pagamento.status = "CRIADO";
+    //   pagamento.data = new Date;
+  
+    //   pagamentoDao.salva(pagamento, function(error, result){
+    //     if(error){
+    //       console.log("Erro ao salvar -->"+error)
+    //       res.status(400).send(error);
+    //     } else {
+    //       console.log('pagamento criado: '+ pagamento.valor);
+    //       res.json(pagamento);
+    //     }
+
+    //   });
+    // });
 }
 
 
